46f0df183510131662f50c7ce2a76d793edc9fb4
Dineshrao0058 <dineshrao.vita@gmail.com>
2024-08-20 14:21:59 +0530

    added cors options

--- products/productRoute.js
+++ products/productRoute.js
@@ -1,17 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('./productController');
-const cors = require('cors');
+const controller = require("./productController");
+const cors = require("cors");
 
 const corsOptions = {
-  origin: 'http://localhost:5000',
+  origin: "http://localhost:5000",
 };
 router.use(cors(corsOptions));
 
-router.post('/addproduct', controller.createProduct);
-router.get('/getproducts', controller.getProducts);
-router.put('/updateproduct/:id', controller.updateProduct);
-router.delete('/deleteproduct/:id', controller.deleteProduct);
-router.get('/search', controller.searchProducts);
+router.post("/addproduct", cors(corsOptions), controller.createProduct);
+router.get("/getproducts", cors(corsOptions), controller.getProducts);
+router.put("/updateproduct/:id", cors(corsOptions), controller.updateProduct);
+router.delete(
+  "/deleteproduct/:id",
+  cors(corsOptions),
+  controller.deleteProduct
+);
+router.get("/search", cors(corsOptions), controller.searchProducts);
 
 module.exports = router;
